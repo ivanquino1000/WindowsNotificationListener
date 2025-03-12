9fcdb18562444bbcb8946652f759c14677bb12dc
ivanquino30@gmail.com <you@example.com>
2025-03-12 18:57:21 -0500

    usp

---
+++ WindowsNotificationHandler/MainPage.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Foundation.Metadata;
+using Windows.UI.Notifications.Management;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace WindowsNotificationHandler
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        
+        public MainPage()
+        {
+            this.InitializeComponent();
+            NotificationHandler notificationHandler = new NotificationHandler();
+            
+        }
+        // Check if the listener is working or supported 
+        static bool CheckListener()
+        {
+            // Supported on Windows Version
+            if (ApiInformation.IsTypePresent("Windows.UI.Notifications.Management.UserNotificationListener"))
+            {
+                // Listener supported!
+                return true;
+            }
+
+            else
+            {
+                // Older version of Windows, no Listener
+                return false;
+            }
+            
+        }
+        async static void RequestAccess()
+        {
+            // Get the listener
+            UserNotificationListener listener = UserNotificationListener.Current;
+
+            // And request access to the user's notifications (must be called from UI thread)
+            UserNotificationListenerAccessStatus accessStatus = await listener.RequestAccessAsync();
+            Debug.WriteLine(accessStatus);
+            switch (accessStatus)
+            {
+                // This means the user has granted access.
+                case UserNotificationListenerAccessStatus.Allowed:
+
+                    // Yay! Proceed as normal
+                    break;
+
+                // This means the user has denied access.
+                // Any further calls to RequestAccessAsync will instantly
+                // return Denied. The user must go to the Windows settings
+                // and manually allow access.
+                case UserNotificationListenerAccessStatus.Denied:
+
+                    // Show UI explaining that listener features will not
+                    // work until user allows access.
+                    break;
+
+                // This means the user closed the prompt without
+                // selecting either allow or deny. Further calls to
+                // RequestAccessAsync will show the dialog again.
+                case UserNotificationListenerAccessStatus.Unspecified:
+
+                    // Show UI that allows the user to bring up the prompt again
+                    break;
+            }
+        }
+    }
+   
+}
---
+++ WindowsNotificationHandler/NotificationHandler.cs
@@ -0,0 +1,198 @@
+﻿using Nito.AsyncEx;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+using Windows.Foundation.Metadata;
+using Windows.UI.Notifications;
+using Windows.UI.Notifications.Management;
+
+namespace WindowsNotificationHandler
+{
+    public class NotificationHandler
+    {
+        private UserNotificationListener listener;
+        public IList<UserNotification> targetNotifications;
+        private HashSet<string> allowedNotificationTitles;
+        public NotificationHandler()
+        {
+            listener = UserNotificationListener.Current;
+            allowedNotificationTitles = new HashSet<string>
+            {
+                "pushbullet: test notification",
+                "yape: confirmacion de pago",
+            };
+            requestNotificationAccess();
+        }
+        // Check if the listener is working or supported 
+        public bool isSupported()
+        {
+            if (ApiInformation.IsTypePresent("Windows.UI.Notifications.Management.UserNotificationListener"))
+            {
+                string SupportedMessage = "Notification Listener Available.\n";
+                Debug.WriteLine(SupportedMessage);
+                return true;
+            }
+
+            else
+            {
+                string errorMessage = "Notification Listener is not supported on this version of Windows.\n";
+                Debug.WriteLine(errorMessage);
+                return false;
+            }
+        }
+        public async Task notificationChangedHandler()
+        {
+            IReadOnlyList<UserNotification> userNotifications = await listener.GetNotificationsAsync(NotificationKinds.Toast);
+
+            foreach (UserNotification userNotification in userNotifications)
+            {
+                if (!isValidNotification(userNotification))
+                {
+                    Debug.Print("Non valid notification");
+                    continue;
+                }
+
+                storeNotification(userNotification);
+            }
+            //WriteNotificationsToFile(targetNotifications, "C:\\Users\\ivan\\Desktop\\notificationListener\notifications.txt");
+        }
+
+        public static void WriteNotificationsToFile(IList<UserNotification> notifications, string fileName)
+        {
+            using (StreamWriter writer = new StreamWriter(fileName))
+            {
+                foreach (var notification in notifications)
+                {
+                    // Write the notification data to the file
+                    writer.WriteLine($"ID: {notification.Id}");
+                    writer.WriteLine(); // Adds a blank line between notifications for readability
+                }
+            }
+
+            Debug.WriteLine("Notifications written to file successfully.");
+        }
+        private void storeNotification(UserNotification notification)
+        {
+            targetNotifications.Add(notification);
+            Debug.WriteLine("targetNotifications: \n" + targetNotifications);
+        }
+        public async Task RequestNotificationAccessAsync()
+        {
+            if (!isSupported())
+            {
+                // Handle the case where notifications aren't supported (you can inform the user or provide an alternative flow)
+                Debug.WriteLine("Notification Listener is not supported.");
+                return; // Prevent further action if not supported
+            }
+
+            try
+            {
+                // Make sure this runs on the UI thread
+                var accessStatus = await listener.RequestAccessAsync();
+
+                switch (accessStatus)
+                {
+                    case UserNotificationListenerAccessStatus.Allowed:
+                        Debug.WriteLine("Notification Listener Allowed!!! \n");
+                        break;
+                    case UserNotificationListenerAccessStatus.Denied:
+                        Debug.WriteLine("Client Denied Notification Listener \n");
+                        await listener.RequestAccessAsync();
+                        // You can show a message or guide the user to manually enable access via settings
+                        break;
+                    case UserNotificationListenerAccessStatus.Unspecified:
+                        Debug.WriteLine("Notification Access Request Unspecified. Show UI to retry.");
+                        // You could prompt the user with an option to open the settings page
+                        break;
+                    default:
+                        Debug.WriteLine("Unexpected access status.");
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions (e.g., user denied access or other unforeseen errors)
+                Debug.WriteLine($"Error requesting notification access: {ex.Message}");
+            }
+        }
+    
+        //event Handler async structure 
+        public async Task requestNotificationAccess()
+        {
+            if (!isSupported())
+            {
+                Environment.Exit(0);
+            }
+            UserNotificationListenerAccessStatus accessStatus = await listener.RequestAccessAsync();
+
+            if (accessStatus == UserNotificationListenerAccessStatus.Allowed)
+            {
+                Debug.WriteLine("Notification Listener Allowed!!! \n");
+            }
+            else
+            {
+                Debug.WriteLine("client Denied Notification Listener \n");
+                //Environment.Exit(0);
+            }
+
+        }
+        public async void readNotifications()
+        {
+            listener = UserNotificationListener.Current;
+            Debug.Print("Notifications Changed Event: \n");
+            IReadOnlyList<UserNotification> userNotifications = await listener.GetNotificationsAsync(NotificationKinds.Toast);
+
+            foreach (UserNotification userNotification in userNotifications)
+            {
+                IList<NotificationBinding> nBindings = userNotification.Notification.Visual.Bindings;
+
+                foreach (var binding in nBindings)
+                {
+                    var elements = binding.GetTextElements();
+                    foreach (var element in elements)
+                    {
+                        // Assuming the element has a 'text' property
+                        var textElement = element.Text;
+                        Debug.WriteLine("Extracted Text: " + textElement);
+                    }
+                }
+
+            }
+        }
+        private string GetNotificationText(UserNotification notification, bool getTitle = true)
+        {
+            NotificationBinding toastBinding = notification.Notification.Visual.GetBinding(KnownNotificationBindings.ToastGeneric);
+            IReadOnlyList<AdaptiveNotificationText> textElements = toastBinding.GetTextElements();
+
+            if (getTitle)
+            {
+                return textElements.FirstOrDefault()?.Text;
+            }
+            else
+            {
+                return string.Join("\n", textElements.Skip(1).Select(t => t.Text));
+            }
+        }
+        public Boolean isValidNotification(UserNotification notification)
+        {
+
+            string titleText = GetNotificationText(notification, true);
+
+            if (allowedNotificationTitles.Contains(titleText))
+            {
+                return true;
+            }
+            else
+            {
+                Debug.Print("Notification not allowed: ", titleText);
+                return false;
+            }
+
+        }
+    }
+}
